1e522439aaba809007500e1282d6148fad64b8c9
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-21 18:50:20 -0300

    structure updated

--- front-on-food/pages/index.tsx
+++ front-on-food/pages/index.tsx
@@ -1,19 +1,19 @@
 import Template from "components/layout/Template";
-import { DishCard, SearchField } from "@/modules";
+import { SearchField } from "@/modules";
 
 export default function Home() {
   return (
     <Template type="home">
-      <h2>Find delivery options next to you.</h2>
+      <h2 className="mb-12 text-center text-xl font-semibold leading-6 text-brand-900 md:text-2xl md:font-bold md:leading-8">
+        Find delivery options next to you.
+      </h2>
 
       <SearchField />
 
-      <h2>
+      <h2 className="mt-12 text-center text-lg font-semibold leading-6 text-brand-900 md:text-xl md:leading-8">
         Find your new favourite delivery. Thousands of options just a click
         away.
       </h2>
-
-      <DishCard />
     </Template>
   );
 }
