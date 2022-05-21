c60ce807b547d1e936b6991633637f9fa6795e95
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-21 17:37:52 -0300

    home page structure updated

--- front-on-food/pages/index.tsx
+++ front-on-food/pages/index.tsx
@@ -1,8 +1,19 @@
 import Template from "components/layout/Template";
-import type { NextPage } from "next";
+import { DishCard, SearchField } from "@/modules";
 
-const Home: NextPage = () => {
-  return <Template>Home</Template>;
-};
+export default function Home() {
+  return (
+    <Template type="home">
+      <h2>Find delivery options next to you.</h2>
 
-export default Home;
+      <SearchField />
+
+      <h2>
+        Find your new favourite delivery. Thousands of options just a click
+        away.
+      </h2>
+
+      <DishCard />
+    </Template>
+  );
+}
