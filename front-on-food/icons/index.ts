97c3afaeb93a5e7bfdde5024b460cb226b605efd
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-21 13:11:30 -0300

    .

---
+++ front-on-food/icons/index.ts
@@ -0,0 +1,19 @@
+import iconBookmark from "../public/images/icons/bookmark.svg";
+import iconCategory from "../public/images/icons/category.svg";
+import iconDelivery from "../public/images/icons/delivery.svg";
+import iconFilter from "../public/images/icons/filter.svg";
+import iconHeart from "../public/images/icons/heart.svg";
+import iconHome from "../public/images/icons/home.svg";
+import iconRestaurant from "../public/images/icons/restaurant.svg";
+import iconReviews from "../public/images/icons/reviews.svg";
+
+export {
+  iconBookmark,
+  iconCategory,
+  iconDelivery,
+  iconFilter,
+  iconHeart,
+  iconHome,
+  iconRestaurant,
+  iconReviews,
+};
