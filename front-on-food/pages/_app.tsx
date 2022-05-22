e3ae74ca56bf4adb90f6cce82bccec7b29f3be51
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-21 22:39:00 -0300

    react query client provider

--- front-on-food/pages/_app.tsx
+++ front-on-food/pages/_app.tsx
@@ -1,8 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
